ed63e376459bf0f735cedb40da1b4de60dd0d22e
David <davidllumitaxi67@gmail.com>
2024-06-24 01:09:43 -0500

    Install ORM

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 import express from 'express'
 import usuarioRouters from './routes/usuarioRoutes.js'
+import db from './config/db.js'
 
 //Crear la app
 const app = express();
+
+//Conexión a la base de datos
+try {
+    await db.authenticate();
+    db.asyn
+    console.log('Conexión Correcta a la DB')
+    
+} catch (error) {
+    console.log('iniiono')
+}
 
 //Habilitar Pug
 app.set('view engine', 'pug')
