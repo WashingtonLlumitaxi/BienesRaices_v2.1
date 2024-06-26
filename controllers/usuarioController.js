53e29af4a3962f5e7a973abc61fef19c5883f1c4
David <davidllumitaxi67@gmail.com>
2024-06-25 23:55:41 -0500

    validation

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -17,15 +17,28 @@ const registrar = async (req, res) => {
     //Validación
     await check('nombre').notEmpty().withMessage('El nombre no puede ir vacios').run(req)
     await check('email').isEmail().withMessage('Esto no parece un email').run(req)
-    await check('password').isLength({min:4}).withMessage('El password debe ser de al menos 4 caracteres').run(req)
-    await check('repetir_password').equals('password').withMessage('Los password no son iguales').run(req)
-    
+    await check('password').isLength({ min: 4 }).withMessage('El password debe ser de al menos 4 caracteres').run(req)
+    let password1 = req.body.password
+    console.log(password1)
+    await check('repetir_password').equals(password1).withMessage('Los password no son iguales').run(req)
 
-    let resultado = validationResult(req)
 
-    res.json(resultado.array())
+    let resultado = validationResult(req) //resultado de la validación
+
+    //Verificar que el resultado este vacio 
+    if (!resultado.isEmpty()) {
+        return res.render('auth/registro', {
+            pagina: 'Crear Cuenta',
+            errores: resultado.array(),
+            usuario: {
+                nombre: req.body.nombre,
+                email: req.body.email
+            }
+        })
+    }
+
     const usuario = await Usuario.create(req.body)
-    res.json(usuario);
+
 }
 
 const formularioOlvidePassword = (req, res) => {
